65305e99feb7dfeaf8fef228a83fd33f65d7c9ec
taichi88 <nikoloz.gabeskiria@gmail.com>
2024-12-08 12:00:46 +0400

    Add project files.

---
+++ WeatherSimulator97vid/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace WeatherSimulator97vid
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of days to simulate");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
