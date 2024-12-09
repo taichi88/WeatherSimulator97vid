1eb2da7cc66c65337a87c0cdccb434b33e5bf174
taichi88 <nikoloz.gabeskiria@gmail.com>
2024-12-09 11:30:34 +0400

    updated

--- WeatherSimulator97vid/Program.cs
+++ WeatherSimulator97vid/Program.cs
@@ -8,6 +8,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the number of days to simulate");
+            int days = int.Parse(Console.ReadLine());
+            int[] temperature = new int[days];
+            string[] conditions = { "Sunny,", "Rainy", "cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-10, 40);
+                Console.WriteLine(temperature[i]);
+
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+                Console.WriteLine(weatherConditions[i]);
+            }
+
+
             Console.WriteLine("Hello, World!");
         }
     }
